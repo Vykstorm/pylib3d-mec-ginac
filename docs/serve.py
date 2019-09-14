8d5bc40012d1026cb6f9a33b8d21e1446265c13b
Vykstorm <victorruizgomezdev@gmail.com>
2019-09-14 15:17:56 +0200

    Added missing code

--- docs/serve.py
+++ docs/serve.py
@@ -12,3 +12,6 @@ app = Flask(__name__, static_url_path='/', static_folder='_build/html/')
 @app.route('/docs/<path:path>')
 def serve_sphinx_docs(path='index.html'):
     return app.send_static_file(path)
+
+if __name__ == '__main__':
+    app.run()
