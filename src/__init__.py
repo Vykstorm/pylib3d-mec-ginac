bb4e9a38cd36d280c77911d85c53593fc500a854
Vykstorm <victorruizgomezdev@gmail.com>
2019-09-19 08:25:00 +0200

    Now variable __all__ is generated automatically

--- src/__init__.py
+++ src/__init__.py
@@ -5,12 +5,22 @@ Public API of the pylib3d_mec_ginac library.
 '''
 
 
-# This variable lists all variables in the public API
-__all__ = [
-    'System', 'Coordinate', 'Velocity', 'Acceleration',
-    'AuxCoordinate', 'AuxVelocity', 'AuxAcceleration',
-    'Parameter', 'JointUnknown', 'Input', 'SymbolNumeric'
-]
+# The next variable will contain all public API methods & classes
+__all__ = []
+
+# Import underline extension
+import lib3d_mec_ginac_ext as _ext
+
+# Get all definitions in the extension (add them to __all__ and globals())
+for name in dir(_ext):
+    if name.startswith('_'):
+        continue
+    obj = getattr(_ext, name)
+    if obj.__module__ != _ext.__name__:
+        continue
+    __all__.append(name)
+    globals()[name] = obj
+
 
 
 # This only has effect with python>=3.7 (PEP 562)
@@ -18,14 +28,7 @@ __all__ = [
 def __getattr__(name):
     if name.startswith('_') or name not in __all__:
         raise AttributeError(f'{name} is not defined in the pylib3d_mec_ginac library API')
-    return getattr(globals(), name)
+    return globals()[name]
 
 def __dir__():
     return __all__.copy()
-
-
-# Import extension definitions
-from lib3d_mec_ginac_ext import System, SymbolNumeric
-from lib3d_mec_ginac_ext import Coordinate, Velocity, Acceleration
-from lib3d_mec_ginac_ext import AuxCoordinate, AuxVelocity, AuxAcceleration
-from lib3d_mec_ginac_ext import Parameter, JointUnknown, Input
