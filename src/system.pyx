4aa5aed209c5ec733db627ff479c78f23e342180
Vykstorm <victorruizgomezdev@gmail.com>
2019-09-22 11:37:58 +0200

    Reimplemented get_symbol, get_symbols & implemented get_symbols_by_type

--- src/system.pyx
+++ src/system.pyx
@@ -4,7 +4,7 @@ Description: This module defines the class System
 '''
 
 
-## Import statements
+######## Imports ########
 
 # Import cython internal library
 cimport cython
@@ -23,23 +23,39 @@ from src.cnumeric cimport numeric as c_numeric
 # Python imports
 from collections.abc import Mapping
 from functools import partial, partialmethod
+from inspect import Signature, Parameter
+from operator import attrgetter
 
 
-## C Type aliases
 
+
+######## C helper methods, variables & types ########
+
+# Type alias representing a list of numeric symbols (std::vector[symbol_numeric*])
 ctypedef c_vector[c_symbol_numeric*] c_symbol_numeric_list
 
 
 
-## Helper variables, methods and types
+
+######## Python helper methods & variables ########
 
 # All numeric symbol types
-_symbol_types = tuple(map(str.encode, (
+_symbol_types = frozenset(map(str.encode, (
     'coordinate', 'velocity', 'acceleration',
     'aux_coordinate', 'aux_velocity', 'aux_acceleration',
     'parameter', 'joint_unknown', 'input'
 )))
 
+# All symbol types that cannot be created by the user (they are generated
+# automatically when other kind of symbols are spawned)
+_derivable_symbol_types = frozenset(map(str.encode, (
+    'velocity', 'acceleration', 'aux_velocity', 'aux_acceleration'
+)))
+
+
+
+
+######## Class definitions ########
 
 
 ## Class which acts like a bridge between Python and C++ System class
@@ -65,73 +81,108 @@ cdef class _System:
 
     ######## Symbol getters ########
 
-    cdef c_map[c_string, c_symbol_numeric_list] _get_c_symbols_by_type(self):
-        ## This method returns C++ map where each entry value is a vector of pointers to symbol_numeric objects
-        # and keys are symbol types.
-        cdef c_map[c_string, c_symbol_numeric_list] symbols_by_type
-        symbols_by_type[b'coordinate'] = self._c_handler.get_Coordinates()
-        symbols_by_type[b'velocity'] = self._c_handler.get_Velocities()
-        symbols_by_type[b'acceleration'] = self._c_handler.get_Accelerations()
-        symbols_by_type[b'aux_coordinate'] = self._c_handler.get_AuxCoordinates()
-        symbols_by_type[b'aux_velocity'] = self._c_handler.get_AuxVelocities()
-        symbols_by_type[b'aux_acceleration'] = self._c_handler.get_AuxAccelerations()
-        symbols_by_type[b'parameter'] = self._c_handler.get_Parameters()
-        symbols_by_type[b'input'] = self._c_handler.get_Inputs()
-        symbols_by_type[b'joint_unknown'] = self._c_handler.get_Joint_Unknowns()
-        return symbols_by_type
+    cdef c_symbol_numeric_list _get_c_symbols_by_type(self, c_string kind):
+        '''
+        Get all symbols of the given type defined within this system
+        :param kind: Kind of symbols to retrieve. e.g: 'parameter'
+        :type kind: std::string
+        :rtype: std::vector[symbol_numeric*]
+        '''
+        if kind == b'coordinate':
+            return self._c_handler.get_Coordinates()
+        if kind == b'velocity':
+            return self._c_handler.get_Velocities()
+        if kind == b'acceleration':
+            return self._c_handler.get_Accelerations()
+        if kind == b'aux_coordinate':
+            return self._c_handler.get_AuxCoordinates()
+        if kind == b'aux_velocity':
+            return self._c_handler.get_AuxVelocities()
+        if kind == b'aux_acceleration':
+            return self._c_handler.get_AuxAccelerations()
+        if kind == b'parameter':
+            return self._c_handler.get_Parameters()
+        if kind == b'input':
+            return self._c_handler.get_Inputs()
+        if kind == b'joint_unknown':
+            return self._c_handler.get_Joint_Unknowns()
 
 
-    cpdef get_symbols(self):
-        symbols = []
-        cdef c_symbol_numeric_list c_symbols
-        cdef c_map[c_string, c_symbol_numeric_list] c_symbols_by_type
-        cdef c_pair[c_string, c_symbol_numeric_list] c_map_entry
+    cdef c_symbol_numeric_list _get_c_symbols(self):
+        '''
+        Get all symbols within this system
+        :rtype: std::vector[symbol_numeric*]
+        '''
+        cdef c_vector[c_symbol_numeric_list] containers
+        containers.push_back(self._c_handler.get_Coordinates())
+        containers.push_back(self._c_handler.get_Velocities())
+        containers.push_back(self._c_handler.get_Accelerations())
+        containers.push_back(self._c_handler.get_AuxCoordinates())
+        containers.push_back(self._c_handler.get_AuxVelocities())
+        containers.push_back(self._c_handler.get_AuxAccelerations())
+        containers.push_back(self._c_handler.get_Parameters())
+        containers.push_back(self._c_handler.get_Inputs())
+        containers.push_back(self._c_handler.get_Joint_Unknowns())
 
-        c_symbols_by_type = self._get_c_symbols_by_type()
-        for c_map_entry in c_symbols_by_type:
-            c_symbols = c_map_entry.second
-            symbols.extend([SymbolNumeric(<Py_ssize_t>c_symbol, (<bytes>c_map_entry.first).decode(), self) for c_symbol in c_symbols])
+        cdef c_symbol_numeric_list symbols
+        cdef size_t num_symbols = 0
+
+        for container in containers:
+            num_symbols += container.size()
+        symbols.reserve(num_symbols)
+
+        for container in containers:
+            symbols.insert(symbols.end(), container.begin(), container.end())
 
         return symbols
 
 
 
     cpdef get_symbol(self, name, kind=None):
-        # Validate input argument types
+        '''get_symbol(name: str[, kind: str]) -> SymbolNumeric
+        Search a numeric symbol defined within this system with the given name and type.
+
+        :param name: Name of the numeric symbol to fetch
+        :param kind: Type of symbol.
+            It can be one None (by default) or one of the next values:
+            'coordinate', 'velocity', 'acceleration',
+            'aux_coordinate', 'aux_velocity', 'aux_acceleration',
+            'parameter', 'input', 'joint_unknown'
+            If set to None, the search is performed over all symbols defined by this system
+            regarding their types.
+        :type name: str
+        :type kind: str
+        :returns: The numeric symbol with that name & type if it exists
+        :rtype: SymbolNumeric
+        '''
+        # Validate arguments types
         if not isinstance(name, (str, bytes)):
-            raise TypeError('Symbol name must be a string or bytes sequence')
+            raise TypeError('Symbol name must be a string or bytes object')
 
         if kind is not None and not isinstance(kind, (str, bytes)):
-            raise TypeError('Symbol type must be a string or bytes sequence')
+            raise TypeError('Symbol type must be a string or bytes object')
 
-        # Convert input arguments to bytes
+        # Validate & parse argument values
         if isinstance(name, str):
             name = name.encode()
 
         if isinstance(kind, str):
             kind = kind.encode()
 
-        # Validate input argument values
         if kind is not None and kind not in _symbol_types:
-            raise ValueError(f'Invalid "{kind}" symbol type')
+            raise ValueError(f'Invalid "{kind.decode()}" symbol type')
 
-        # Find the numeric symbol by name
-        cdef c_map[c_string, c_symbol_numeric_list] c_symbols_by_type = self._get_c_symbols_by_type()
-        cdef c_pair[c_string, c_symbol_numeric_list] c_map_entry
+        # Find a numeric symbol by name
         cdef c_symbol_numeric_list c_symbols
-        cdef c_symbol_numeric* c_symbol
-
         if kind is None:
-            for c_map_entry in c_symbols_by_type:
-                for c_symbol in c_map_entry.second:
-                    if c_symbol.get_name() == <c_string>name:
-                        return SymbolNumeric(<Py_ssize_t>c_symbol, (<bytes>c_map_entry.first).decode(), self)
+            c_symbols = self._get_c_symbols()
         else:
-            # Get symbols of an specific type
-            c_symbols = c_symbols_by_type[<c_string>kind]
-            for c_symbol in c_symbols:
-                if c_symbol.get_name() == <c_string>name:
-                    return SymbolNumeric(<Py_ssize_t>c_symbol, kind.decode(), self)
+            c_symbols = self._get_c_symbols_by_type(kind)
+
+        cdef c_symbol_numeric* c_symbol
+        for c_symbol in c_symbols:
+            if c_symbol.get_name() == <c_string>name:
+                return SymbolNumeric(<Py_ssize_t>c_symbol)
 
         # No symbol with such name exists
         if kind is None:
@@ -140,9 +191,53 @@ cdef class _System:
 
 
 
+    cpdef get_symbols(self):
+        '''get_symbols() -> Dict[str, SymbolNumeric]
+        Get all symbols defined within this system
+
+        :returns: Returns all the symbols defined in a dictionary, where keys are
+            symbol names and values, instances of the class SymbolNumeric
+        :rtype: Dict[str, SymbolNumeric]
+        '''
+
+        cdef c_symbol_numeric_list c_symbols = self._get_c_symbols()
+        symbols = [SymbolNumeric(<Py_ssize_t>c_symbol) for c_symbol in c_symbols]
+        return dict(zip(map(attrgetter('name'), symbols), symbols))
+
+
+    cpdef get_symbols_by_type(self, kind):
+        '''get_symbols_by_type(kind: str) -> Dict[str, SymbolNumeric]
+        Get all symbols of the given type defined within this system
+
+        :returns: All symbols with the given type in a dictionary, where keys are
+            symbol names and values, instances of the class SymbolNumeric
+        :rtype: Dict[str, SymbolNumeric]
+        '''
+        if not isinstance(kind, (str, bytes)):
+            raise TypeError('Symbol type must be a string or bytes object')
+        if isinstance(kind, str):
+            kind = kind.encode()
+        if kind not in _symbol_types:
+            raise ValueError(f'Invalid "{kind.decode()}" symbol type')
+
+
+        cdef c_symbol_numeric_list c_symbols = self._get_c_symbols_by_type(kind)
+        symbols = [SymbolNumeric(<Py_ssize_t>c_symbol) for c_symbol in c_symbols]
+        return dict(zip(map(attrgetter('name'), symbols), symbols))
+
+
+
+
+    ######## Symbol constructors ########
+
+
+
 
 ## System class for Python (it emulates the class System in C++ but also provides additional features).
 class System(_System):
+
+    ######## Get/Set symbol value ########
+
     def get_value(self, symbol):
         if not isinstance(symbol, (SymbolNumeric, str, bytes)):
             raise TypeError(f'Input argument must be a string or an instance of the class {SymbolNumeric.__name__}')
@@ -161,6 +256,8 @@ class System(_System):
         symbol.set_value(value)
 
 
+    ######## Misc methods ########
+
     def has_symbol(self, *args, **kwargs):
         try:
             self.get_symbol(*args, **kwargs)
@@ -169,8 +266,24 @@ class System(_System):
         return True
 
 
-# Autogenerate getter_* and has_* methods
+    ######## Metamethods ########
+
+    def __contains__(self, symbol):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+
+# Autogenerate get_*, new_* and has_* methods
 for symbol_type in _symbol_types:
     name = symbol_type.decode()
     setattr(System, 'get_' + name, partialmethod(System.get_symbol, kind=symbol_type))
     setattr(System, 'has_' + name, partialmethod(System.has_symbol, kind=symbol_type))
+
+    #if symbol_type not in _derivable_symbol_types:
+    #    setattr(System, 'new_' + name, partialmethod(System._new_symbol, symbol_type))
