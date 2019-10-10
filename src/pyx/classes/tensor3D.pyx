4858aac98337b7b2f303eac1e60d102e7cc71e94
Vykstorm <victorruizgomezdev@gmail.com>
2019-10-10 23:21:23 +0200

    Added docstrings to methods of the class Tensor3D

--- src/pyx/classes/tensor3D.pyx
+++ src/pyx/classes/tensor3D.pyx
@@ -79,6 +79,9 @@ cdef class Tensor3D(Matrix):
 
 
     def __neg__(self):
+        '''
+        Returns this tensor negated.
+        '''
         return _tensor_from_c_value(
             Expr(-1)._c_handler *\
             c_deref(<c_Tensor3D*>self._get_c_handler())
@@ -86,6 +89,10 @@ cdef class Tensor3D(Matrix):
 
 
     def __pos__(self):
+        '''
+        Perform the unary positive operation on this tensor. The result is another
+        tensor.
+        '''
         return _tensor_from_c_value(c_deref(<c_Tensor3D*>self._get_c_handler()))
 
 
@@ -95,6 +102,10 @@ cdef class Tensor3D(Matrix):
 
 
     def __add__(Tensor3D self, other):
+        '''
+        Performs the sum operation between two tensors. The result is also a
+        tensor.
+        '''
         if not isinstance(other, Tensor3D):
             raise TypeError(f'Unsupported operand type for +: Tensor3D and {type(other).__name__}')
 
@@ -105,6 +116,10 @@ cdef class Tensor3D(Matrix):
 
 
     def __sub__(Tensor3D self, other):
+        '''
+        Performs the subtraction operation between two tensors. The result is also
+        a tensor.
+        '''
         if not isinstance(other, Tensor3D):
             raise TypeError(f'Unsupported operand type for -: Tensor3D and {type(other).__name__}')
 
@@ -115,6 +130,15 @@ cdef class Tensor3D(Matrix):
 
 
     def __mul__(left_op, right_op):
+        '''
+        Performs the product operation between two tensors, a tensor and vector or
+        a tensor and a expression.
+        * If both operands are tensors, the result is a tensor
+        * If the first operand is a tensor and the second a vector, the result is a new
+            vector
+        * If one of the operands is a tensor and the other is an expression, perform
+            the scalar product between them. The result is also a tensor.
+        '''
         if isinstance(left_op, Tensor3D) and isinstance(right_op, Tensor3D):
             return _tensor_from_c_value(
                 c_deref(<c_Tensor3D*>(<Tensor3D>left_op)._get_c_handler()) *\
@@ -124,14 +148,15 @@ cdef class Tensor3D(Matrix):
         if not isinstance(left_op, Tensor3D) and not isinstance(right_op, Tensor3D):
             raise TypeError(f'Unsupported operand type for *: {type(left_op).__name__} and {type(right_op).__name__}')
 
-        if not isinstance(left_op, Tensor3D):
-            left_op, right_op = right_op, left_op
-
         if isinstance(right_op, Vector3D):
             return _vector_from_c_value(
                 c_deref(<c_Tensor3D*>(<Tensor3D>left_op)._get_c_handler()) *\
                 c_deref(<c_Vector3D*>(<Vector3D>right_op)._get_c_handler())
             )
+
+        if not isinstance(left_op, Tensor3D):
+            left_op, right_op = right_op, left_op
+
 
         right_op = Expr(right_op)
         return _tensor_from_c_value(
@@ -141,6 +166,10 @@ cdef class Tensor3D(Matrix):
 
 
     def __truediv__(Tensor3D self, other):
+        '''
+        Performs the division operation between this tensor and a expression.
+        The result is another expression.
+        '''
         if not isinstance(other, Expr):
             expr = Expr(other)
         inverted_expr = 1 / expr
