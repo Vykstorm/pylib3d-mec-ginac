edbda91e102488683a3e87fc6f4b077f71771774
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-07 19:29:02 +0100

    Refactorized code

--- src/pyx/classes/matrix.pyx
+++ src/pyx/classes/matrix.pyx
@@ -984,39 +984,31 @@ cdef class Matrix(Object):
 
 
 
+    ######## Creation routines ########
 
-
-NamedObject.register(Matrix)
-LatexRenderable.register(Matrix)
-
-
-
-######## Matrix3 class ########
-
-
-class Matrix3(Matrix):
-    '''
-    Instances of this class represents symbolic matrices with fixed size (3x3)
-    '''
-    def __init__(self, values=None):
-        super().__init__(shape=(3, 3), values=values)
 
     @classmethod
-    def eye(cls):
+    def eye(cls, n):
         '''eye() -> Matrix3
-        Get a 3x3 identity matrix
+        Create a symbolic identity matrix of size nxn
         '''
-        return cls(np.eye(3))
+        if not isinstance(n, int) or n <= 0:
+            raise TypeError('n must be a number greater than zero')
+        return cls(np.eye(n))
+
+
+
+    ######## Rotation matrices ########
 
 
     @classmethod
     def xrot(cls, phi):
-        '''rot_x(phi: Expr) -> Matrix3
+        '''rot_x(phi: Expr) -> Matrix
         Get a 3x3 matrix transformation which represents a rotation of ``phi`` radians
         with respect the x axis
 
         :type phi: Expr
-        :rtype: Matrix3
+        :rtype: Matrix
 
         '''
         return cls.rot([1, 0, 0], phi)
@@ -1024,39 +1016,41 @@ class Matrix3(Matrix):
 
     @classmethod
     def yrot(cls, phi):
-        '''rot_y(phi: Expr) -> Matrix3
+        '''rot_y(phi: Expr) -> Matrix
         Get a 3x3 matrix transformation which represents a rotation of ``phi`` radians
         with respect the y axis
 
         :type phi: Expr
-        :rtype: Matrix3
+        :rtype: Matrix
 
         '''
         return cls.rot([0, 1, 0], phi)
 
 
+
     @classmethod
     def zrot(cls, phi):
-        '''rot_z(phi: Expr) -> Matrix3
+        '''rot_z(phi: Expr) -> Matrix
         Get a 3x3 matrix transformation which represents a rotation of ``phi`` radians
         with respect the z axis
 
         :type phi: Expr
-        :rtype: Matrix3
+        :rtype: Matrix
 
         '''
         return cls.rot([0, 0, 1], phi)
 
 
+
     @classmethod
     def rot(cls, axis, phi):
-        '''rot(axis: Matrix, phi: Expr) -> Matrix3
+        '''rot(axis: Matrix, phi: Expr) -> Matrix
         Get a 3x3 matrix transformation which represents a rotation of ``phi`` radians
         with respect the given axis
 
         :type axis: Matrix
         :type phi: Expr
-        :rtype: Matrix3
+        :rtype: Matrix
 
         '''
         if not isinstance(axis, Matrix):
@@ -1068,4 +1062,11 @@ class Matrix3(Matrix):
             phi = Expr(phi)
 
         axis_skew = axis.get_skew()
-        return cls.eye() + sin(phi) * axis_skew + (1 - cos(phi)) * (axis_skew * axis_skew)
+        return cls.eye(3) + sin(phi) * axis_skew + (1 - cos(phi)) * (axis_skew * axis_skew)
+
+
+
+
+
+NamedObject.register(Matrix)
+LatexRenderable.register(Matrix)
