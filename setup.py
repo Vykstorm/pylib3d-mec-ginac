18bd9225fcfd9af75d52b26886c8afaed0908744
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-07 13:40:56 +0100

    Renamed module

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ EXTENSION_DEFINITION_FILES = list(map(partial(join, 'src/pyx'), chain(
 
     # Modules at src/pyx/classes
     map(partial(join, 'classes'), [
-        'object.pyx', 'system.pyx', 'symbol_numeric.pyx', 'expression.pyx',
+        'object.pyx', 'system.pyx', 'symbol.pyx', 'expression.pyx',
         'base.pyx', 'matrix.pyx', 'vector3D.pyx', 'tensor3D.pyx',
         'point.pyx', 'frame.pyx', 'solid.pyx', 'wrench3D.pyx', 'drawing3D.pyx'
     ])
