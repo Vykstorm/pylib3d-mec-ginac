c156d6e461838ff4d332f9b7534c28d40bfcc30e
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-16 22:52:21 +0100

    Updated public API

--- src/__init__.py
+++ src/__init__.py
@@ -70,7 +70,7 @@ for name in dir(System):
     globals()[name] = _create_system_global_func(getattr(System, name))
 
 # Expose draw_* methods of the default system object (add them to __all__ and globals())
-from .drawing.viewer import Viewer
+from .drawing.viewer import Viewer, Drawing3D
 
 def _create_viewer_global_func(method):
     @wraps(method)
@@ -88,7 +88,7 @@ for name in dir(Viewer):
     __all__.append(name)
     globals()[name] = _create_viewer_global_func(getattr(Viewer, name))
 
-__all__.append('Viewer')
+__all__.extend(['Viewer', 'Drawing3D'])
 
 
 
