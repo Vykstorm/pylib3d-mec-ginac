fad9e017caac8cd0eec8189b7f3ecc829e018992
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-16 11:31:25 +0100

    Updated FrameDrawing class

--- src/drawing/frame.py
+++ src/drawing/frame.py
@@ -10,6 +10,9 @@ This file defines the class FrameDrawing
 from .geometry import _create_sphere_geometry, _create_arrow_geometry, _create_assembly_geometry
 from .drawing import Drawing3D
 from vtk import vtkAssembly, vtkSphereSource, vtkPolyDataMapper, vtkActor
+from collections.abc import Iterable
+from itertools import repeat
+
 
 
 ######## class FrameDrawing ########
@@ -23,13 +26,24 @@ class FrameDrawing(Drawing3D):
         axis_shaft_resolution=10, axis_tip_resolution=25, origin_radius=0.06, origin_resolution=15):
 
         # Validate input arguments
+        if isinstance(axis_size, Iterable):
+            axis_size = tuple(axis_size)
+            if len(axis_size) != 3:
+                raise ValueError
+        else:
+            axis_size = tuple(repeat(axis_size, 3))
+
 
 
         # Create geometry
         origin = _create_sphere_geometry(origin_radius, origin_resolution)
-        x_axis, x_axis_shaft, x_axis_tip = _create_arrow_geometry()
-        y_axis, y_axis_shaft, y_axis_tip = _create_arrow_geometry()
-        z_axis, z_axis_shaft, z_axis_tip = _create_arrow_geometry()
+        x_axis_shaft, x_axis_tip = _create_arrow_geometry(size=axis_size[0])
+        y_axis_shaft, y_axis_tip = _create_arrow_geometry(size=axis_size[1])
+        z_axis_shaft, z_axis_tip = _create_arrow_geometry(size=axis_size[2])
+
+        x_axis = _create_assembly_geometry(x_axis_shaft, x_axis_tip)
+        y_axis = _create_assembly_geometry(y_axis_shaft, y_axis_tip)
+        z_axis = _create_assembly_geometry(z_axis_shaft, z_axis_tip)
 
         x_axis_tip.GetProperty().SetColor(1, 0, 0)
         y_axis_tip.GetProperty().SetColor(0, 1, 0)
@@ -38,7 +52,9 @@ class FrameDrawing(Drawing3D):
         y_axis.SetOrientation(0, 0, 90)
         z_axis.SetOrientation(0, -90, 0)
 
-        frame = _create_assembly_geometry(origin, x_axis, y_axis, z_axis)
+        frame = _create_assembly_geometry(
+            origin, x_axis, y_axis, z_axis
+        )
 
 
         # Initialize super instance
