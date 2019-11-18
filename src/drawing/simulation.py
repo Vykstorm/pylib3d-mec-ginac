cd4ffd1e9ac67e6b350155500e3246c0d1236c9a
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-18 20:46:44 +0100

    Methods of the class simulation are now thread safe

--- src/drawing/simulation.py
+++ src/drawing/simulation.py
@@ -2,7 +2,7 @@
 
 from .timer import Timer
 from time import time
-
+from threading import RLock
 
 
 
@@ -23,92 +23,118 @@ class Simulation:
         self._update_freq = 30
         self._timer = None
         self._elapsed_time, self._last_update_time = 0.0, None
+        self._lock = RLock()
 
 
     def start(self):
-        if self._state != 'stopped':
-            raise RuntimeError('Simulation already started')
-        self._state = 'running'
-        self._timer = Timer(self.update, interval=1 / self._update_freq)
-        self._timer.start(resumed=True)
+        with self._lock:
+            if self._state != 'stopped':
+                raise RuntimeError('Simulation already started')
+            self._state = 'running'
+            self._timer = Timer(self.update, interval=1 / self._update_freq)
+            self._timer.start(resumed=True)
 
 
     def resume(self):
-        if self._state != 'paused':
-            raise RuntimeError('Simulation is not paused')
-        self._state = 'running'
+        with self._lock:
+            if self._state != 'paused':
+                raise RuntimeError('Simulation is not paused')
+            self._state = 'running'
 
 
     def pause(self):
-        if self._state != 'running':
-            raise RuntimeError('Simulation is not running')
-        self._state = 'paused'
+        with self._lock:
+            if self._state != 'running':
+                raise RuntimeError('Simulation is not running')
+            self._state = 'paused'
 
 
     def stop(self):
-        if self._state == 'stopped':
-            raise RuntimeError('Simulation has not started yet')
-        self._state = 'stopped'
-        self._timer.kill()
-        self._timer = None
-        self._elapsed_time = 0.0
-        self._last_update_time = None
-        self.update()
+        with self._lock:
+            if self._state == 'stopped':
+                raise RuntimeError('Simulation has not started yet')
+            self._state = 'stopped'
+            self._timer.kill()
+            self._timer = None
+            self._elapsed_time = 0.0
+            self._last_update_time = None
+            self.update()
 
 
     def is_running(self):
-        return self._state == 'running'
+        with self._lock:
+            return self._state == 'running'
 
 
     def is_paused(self):
-        return self._state == 'paused'
+        with self._lock:
+            return self._state == 'paused'
 
 
     def is_stopped(self):
-        return self._state == 'stopped'
+        with self._lock:
+            return self._state == 'stopped'
 
 
     def get_elapsed_time(self):
-        return self._elapsed_time
+        with self._lock:
+            return self._elapsed_time
 
 
     def get_time_multiplier(self):
-        return self._time_multiplier
+        with self._lock:
+            return self._time_multiplier
 
 
     def set_time_multiplier(self, multiplier):
-        self._time_multiplier = multiplier
+        try:
+            multiplier = float(multiplier)
+            if multiplier <= 0:
+                raise TypeError
+        except TypeError:
+            raise TypeError('Input argument must be a number greater than zero')
+        with self._lock:
+            self._time_multiplier = multiplier
 
 
     def get_update_frequency(self):
-        return self._update_freq
+        with self._lock:
+            return self._update_freq
 
 
     def set_update_frequency(self, frequency):
-        self._update_freq = frequency
-        if self._state != 'stopped':
-            self._timer.set_time_interval(1 / self._update_freq)
+        try:
+            frequency = float(frequency)
+            if frequency <= 0:
+                raise TypeError
+        except TypeError:
+            raise TypeError('Input argument must be a number greater than zero')
+        with self._lock:
+            self._update_freq = frequency
+            if self._state != 'stopped':
+                self._timer.set_time_interval(1 / self._update_freq)
 
 
     def update(self):
-        if self._state == 'running':
-            # Update elapsed time
-            current_time = time()
-            if self._last_update_time is None:
-                self._last_update_time = current_time
-            else:
-                self._elapsed_time += current_time - self._last_update_time
-                self._last_update_time = current_time
+        with self._lock:
+            if self._state == 'running':
+                # Update elapsed time
+                current_time = time()
+                if self._last_update_time is None:
+                    self._last_update_time = current_time
+                else:
+                    self._elapsed_time += current_time - self._last_update_time
+                    self._last_update_time = current_time
 
 
-        # Update time variable
-        elapsed_time = self._elapsed_time * self._time_multiplier
-        system = self._system
-        t = system.get_time()
-        t.set_value(elapsed_time)
+            # Update time variable
+            elapsed_time = self._elapsed_time * self._time_multiplier
+            system = self._system
+            t = system.get_time()
+            t.set_value(elapsed_time)
 
-        # Intergrate
-        # TODO
+            # Intergrate
+            # TODO
 
         # Update scene (update drawings & redraw)
         self._scene.update()
