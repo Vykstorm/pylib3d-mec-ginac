a9cfcc4f44f376c750bb5918e85b2864f54850da
Vykstorm <victorruizgomezdev@gmail.com>
2019-11-20 20:45:03 +0100

    Added docstrings and updated code

--- src/drawing/object.py
+++ src/drawing/object.py
@@ -13,7 +13,19 @@ from inspect import isclass
 
 class Object:
     '''
-    Base class for the rest of classes in the submodule 'drawing'.
+    Instances of this class are used to implemented the observer pattern:
+    - An instance of this class can have an arbitrary number of children objects
+    (also Object instances) and only one parent (optional).
+    An object can be seen in other way as a tree where nodes are objects.
+
+    - Objects can trigger events. Events are propagated from bottom to the top of the object
+    hierachy.
+
+    - All methods on this class are thread safe (You can also use the internal threading
+    lock of the object to perform synchronization for a custom task on the given object)
+    This class implements the "with" protocol with the metamethods __enter__ and __exit__
+    to provide a scope where the internal lock of the object is locked
+
     '''
     def __init__(self):
         super().__init__()
@@ -23,17 +35,36 @@ class Object:
 
 
     def get_parent(self):
+        '''get_parent() -> Object | None
+        Get the parent of this object if any. None otherwise
+
+        :rtype: Object | None
+
+        '''
         with self:
             return self._parent
 
 
     def has_parent(self):
+        '''has_parent() -> bool
+        Returns True if this object has parent. False otherwise.
+
+        :rtype: bool
+
+        '''
         with self:
             return self._parent is not None
 
 
     def get_ancestor(self, kind):
-        #assert isclass(kind)
+        '''get_ancestor(kind) -> Object | None
+        Get the closest ancestor 'x' of this object such that isinstance(x, kind) is
+        evaluated to True. Returns None no ancestor evaluates such predicate to True or
+        this object has no parent.
+
+        :rtype: Object | None
+
+        '''
         with self:
             parent = self._parent
             if parent is None:
@@ -42,7 +73,10 @@ class Object:
 
 
     def get_children(self, kind=None):
-        #assert kind is None or isclass(kind)
+        '''get_children([kind]) -> List[Object]
+        Get all the child objects of this one with the given type. If kind is None
+        returns all the childs.
+        '''
         with self:
             if kind is None:
                 return tuple(self._children)
@@ -50,6 +84,15 @@ class Object:
 
 
     def add_child(self, child):
+        '''add_child(child: Object)
+        Adds a new child to this object.
+
+        :type child: Object
+
+        .. note::
+            Once the child was added to this object, it fires a 'object_entered' event.
+
+        '''
         assert isinstance(child, Object)
         with self:
             self._children.append(child)
@@ -59,6 +102,15 @@ class Object:
 
 
     def remove_child(self, child):
+        '''remove_child(child: Object)
+        Removes a child from this object
+
+        :type child: Object
+
+        .. note::
+            Before the given child is remove from this object, it fires a 'object_exit' event.
+
+        '''
         assert isinstance(child, Object)
         with self:
             if child not in self._children:
@@ -70,18 +122,25 @@ class Object:
 
 
 
-    def add_event_handler(self, callback, event_type=None, args=[], kwargs={}):
+    def add_event_handler(self, callback, event_type=None):
+        '''add_event_handler(callback: Callable[, event_type: str])
+        Add a new handler that will listen the events fired by this object or any of its predecessors.
+
+        :param callback: A callable object that will be invoked when a event is triggered by this object
+            or any of its predecessors. It will receive the kind of event and the object who fired the
+            event as positional arguments.
+
+        :param event_type: Type of events to listen. None to listen to any kind.
+
+        .. seealso:: :func:`fire_event`
+
+        '''
         assert callable(callback)
         assert event_type is None or isinstance(event_type, str)
-        assert isinstance(args, Iterable) and isinstance(kwargs, Mapping)
-
-        args, kwargs = tuple(args), dict(kwargs)
 
         event_handler = SimpleNamespace(
             callback=callback,
-            event_type=event_type,
-            args=args,
-            kwargs=kwargs
+            event_type=event_type
         )
         with self:
             self._event_handlers.append(event_handler)
@@ -89,6 +148,13 @@ class Object:
 
 
     def remove_event_handler(self, callback, event_type=None):
+        '''remove_event_handler(callback: Callable[, event_type: str])
+        Remove all the handlers listening to this object events with the given callback.
+
+        :param event_type: If None, remove all handlers having the provided callback.
+            Otherwise, removes only the ones listening the given event type.
+
+        '''
         assert callable(callback)
         assert event_type is None or isinstance(event_type, str)
 
@@ -116,6 +182,23 @@ class Object:
 
 
     def fire_event(self, event_type, *args, **kwargs):
+        '''fire_event(event_type: str, ...)
+        Fires an event and propagate it to the top of the object hierachy.
+
+        :param event_type: Kind of event to be fired.
+        :param args: Additional positional arguments that will receive the event handlers listening
+            for the event fired
+        :param kwargs: Additional keyword arguments that will receive the event handlers listening
+            for the event fired
+
+        .. note::
+            The event is propagated from the bottom to the top of the object hierachy.
+            If an event handler listening for the event fired is invoked and returns True,
+            the event will be cancelled and no more event handlers will be triggered.
+
+        .. seealso:: :func:`add_event_handler`
+
+        '''
         assert isinstance(event_type, str)
         self._fire_event(self, event_type, *args, **kwargs)
 
@@ -127,6 +210,13 @@ class Object:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._lock.release()
+
+
+
+
+class VtkObjectWrapper(Object):
+    pass
+
 
 
 
