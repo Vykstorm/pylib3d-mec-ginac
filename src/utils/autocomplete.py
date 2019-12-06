5e43681660f2253236879a3c9d1e612fc1746b00
Vykstorm <victorruizgomezdev@gmail.com>
2019-12-06 19:39:59 +0100

    Improved autocomplete text feature

--- src/utils/autocomplete.py
+++ src/utils/autocomplete.py
@@ -6,8 +6,12 @@ Description: This file defines the function autocomplete
 ######## Import statements ########
 
 from collections.abc import Mapping
+from collections import ChainMap
 from operator import methodcaller
-
+from re import match
+import builtins
+import keyword
+from itertools import chain, filterfalse
 
 
 
@@ -19,4 +23,22 @@ def autocomplete(text, context):
     if not isinstance(context, Mapping):
         raise TypeError('context must be a mapping')
 
-    return tuple(filter(methodcaller('startswith', text), context.keys()))
+    m = match('(\w+)\.(\w*)$', text)
+    if m:
+        try:
+            obj = eval(m.group(1))
+            text, context = m.group(2), obj.__dict__
+        except:
+            pass
+    else:
+        context = ChainMap(context, builtins.__dict__)
+
+    if text:
+        names = filter(methodcaller('startswith', text), context.keys())
+    else:
+        names = filterfalse(methodcaller('startswith', '_'), context.keys())
+    names = tuple(names)
+
+
+    results = [name+'(' if callable(value) else name for name, value in zip(names, map(context.__getitem__, names))]
+    return results
