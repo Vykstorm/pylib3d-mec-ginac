2c58293419e2d427a2699fe31349e79ec5da214a
Vykstorm <victorruizgomezdev@gmail.com>
2019-12-14 20:19:49 +0100

    Updated docstrings and improved numeric function evaluation

--- src/core/pyx/numeric.pyx
+++ src/core/pyx/numeric.pyx
@@ -5,6 +5,8 @@ This module defines the class NumericFunction
 '''
 
 
+
+
 ######## Class NumericFunction ########
 
 class NumericFunction:
@@ -19,37 +21,52 @@ class NumericFunction:
     ######## Constructor ########
 
     def __init__(self, atoms, outputs, globals=None):
+        '''
+        Initialize the numeric function. This class is not intended to be instantiated
+        by the user directly.
+
+        :param atoms: Must be a dictionary where keys are atom names (valid python identifiers
+            also), and values will be the expressions assigned to each atom
+            (they must be non empty strings)
+
+        :param outputs: Must be a non empty list of lists representing the outputs of the numeric function.
+            All sublists may have the same number of items (at least one). Also each item must be
+            a non empty string that will be the expression to be evaluated for each output.
+
+        :param globals: Additional global functions needed to evaluate the atoms or the outputs
+            of the numeric function (by default only sin, cos, tan can be used)
+
+
+            :Example:
+
+            >>> func = NumericFunction(
+            >>>     atoms={'atom1': 'x ** 2', 'atom2': 'y ** 2', 'atom3': 'z ** 2'},
+            >>>     outputs=[
+            >>>         ['atom1 * x + y * z', 'atom2 * (y - z)'],
+            >>>         ['atom3 * z - 1', 'atom1 + atom3 * y + 1']
+            >>>     ]
+            >>> )
+            >>> func({'x'=1.0, 'y'=2.0, 'z'=3.0})
+            array([[ 7., -4.],
+                   [26., 20.]])
+
+
+        '''
         ## Validate input arguments
-
-        # Validate atoms argument
-        assert isinstance(atoms, dict)
-        assert all(map(lambda key: isinstance(key, str) and key.isidentifier(), atoms.keys()))
-        assert all(map(lambda value: isinstance(value, str) and value, atoms.values()))
-
-        # Validate outputs argument
-        assert isinstance(outputs, (list, tuple))
-        output = tuple(outputs)
-        assert output
-        assert all(map(lambda output: isinstance(output, (list, tuple)), outputs))
-        outputs = tuple(map(tuple, outputs))
-        assert len(frozenset(map(len, outputs))) == 1
-        assert outputs[0]
-        for output in outputs:
-            assert all(map(lambda item: isinstance(item, str), output)) and all(output)
-
-        # Validate globals argument
-        assert globals is None or isinstance(globals, dict)
-        if globals is not None:
-            assert all(map(lambda key: isinstance(key, str) and key.isidentifier(), globals.keys()))
-        else:
+        if globals is None:
             globals = {'sin': math.sin, 'cos': math.cos, 'tan': math.tan}
-
 
         # Initialize internal fields
         self._atoms = atoms
         self._outputs = outputs
         self._code = None
         self._globals = globals
+        self._outputs_array = np.zeros(self.get_outputs_shape(), dtype=np.float64)
+        self._locals = {'__output__': self._outputs_array}
+
+        # Compile numeric function body if needed
+        if self._code is None:
+            self._compile()
 
 
 
@@ -58,10 +75,9 @@ class NumericFunction:
         # This private method is used to compile the internal numeric function
         lines = [f'{name} = {value}' for name, value in self.atoms.items()]
 
-        if isinstance(self.outputs[0], tuple):
-            lines.append(f'__output__ = [' + ', '.join(['[' + ', '.join(row) + ']' for row in self.outputs]) + ']')
-        else:
-            lines.append(f'__output__ = [{", ".join(self.outputs)}]')
+        n, m = self.outputs_shape
+        outputs = chain.from_iterable(self._outputs)
+        lines.append(f'__output__.flat = [ {", ".join( outputs )} ]')
         source = '\n'.join(lines)
         self._code = compile(source, '<string>', 'exec', optimize=2)
 
@@ -81,8 +97,10 @@ class NumericFunction:
         '''
         return self._atoms
 
+
+
     def get_outputs(self):
-        '''get_outputs() -> List[str]
+        '''get_outputs() -> List[List[str]]
         Get the list of output expressions for this numeric function
 
         :rtype: List[str]
@@ -91,13 +109,23 @@ class NumericFunction:
         return self._outputs
 
 
+    def get_outputs_shape(self):
+        '''get_outputs_shape() -> int, int
+        Get the number of rows and columns of the numpy output arrays when evaluating
+        this function.
+
+        :rtype: int, int
+
+        '''
+        return len(self._outputs), len(self._outputs[0])
+
+
 
     def get_globals(self):
         '''get_globals() -> Dict[str, Any]
         Get the global variables and functions used by this numeric function
         '''
         return self._globals
-
 
 
 
@@ -142,34 +170,15 @@ class NumericFunction:
 
     ######## Function evaluation ########
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, inputs={}):
         '''
-        Evaluate this numeric function with the provided positional & keyword arguments
+        Evaluate this numeric function with the provided inputs.
         '''
-        if args:
-            assert len(args) == 1
-            inputs = args[0]
-            assert isinstance(inputs, dict)
-            assert all(map(lambda key: isinstance(key, str) and key, inputs.keys()))
-        else:
-            inputs = {}
-
-        inputs.update(kwargs)
-        assert all(map(lambda value: isinstance(value, float), inputs.values()))
-
-        # Compile numeric function body if needed
-        if self._code is None:
-            self._compile()
-
-        # Evaluate the function
         globals = {}
         globals.update(self._globals)
         globals.update(inputs)
-        locals = {}
-
-        exec(self._code, globals, locals)
-        result = np.array(locals['__output__'], dtype=np.float64)
-        return result
+        exec(self._code, globals, self._locals)
+        return self._outputs_array
 
 
 
@@ -192,6 +201,19 @@ class NumericFunction:
         Only read property that returns the outputs of this numeric function
         '''
         return self.get_outputs()
+
+
+    @property
+    def outputs_shape(self):
+        '''
+        Only read property that returns the shape of the numpy output arrays
+        when evaluating this numeric function
+
+        :rtype: int, int
+
+        '''
+        return self.get_outputs_shape()
+
 
     @property
     def globals(self):
