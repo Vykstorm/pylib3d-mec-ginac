12fce11fa4d453d9b682c2551f849af84f1e2d75
Vykstorm <victorruizgomezdev@gmail.com>
2020-01-16 15:00:02 +0100

    Added class Vector3

--- src/drawing/vector.py
+++ src/drawing/vector.py
@@ -1,6 +1,6 @@
 '''
 Author: Víctor Ruiz Gómez
-Description: This file defines the class Vector, Vector2
+Description: This file defines the class Vector, Vector2 and Vector3
 '''
 
 
@@ -90,3 +90,45 @@ class Vector2(Vector):
     @y.setter
     def y(self, value):
         self.__setitem__(1, value)
+
+
+
+
+######## class Vector3 ########
+
+class Vector3(Vector):
+    def __init__(self, *args):
+        super().__init__(n=3)
+        if args:
+            self.set(*args)
+
+    def set(self, *args):
+        if len(args) not in (1, 3):
+            raise TypeError('Invalid number of arguments specified')
+        if len(args) == 1:
+            args = args[0]
+        self.__setitem__(slice(0,3), args)
+
+    @property
+    def x(self):
+        return self.__getitem__(0)
+
+    @x.setter
+    def x(self, value):
+        self.__setitem__(0, value)
+
+    @property
+    def y(self):
+        return self.__getitem__(1)
+
+    @y.setter
+    def y(self, value):
+        self.__setitem__(1, value)
+
+    @property
+    def z(self):
+        return self.__getitem__(2)
+
+    @z.setter
+    def z(self, value):
+        self.__setitem__(2, value)
