afd266f5e0f4fed54e24b76f36c47f6c8536664d
Vykstorm <victorruizgomezdev@gmail.com>
2020-01-26 18:41:42 +0100

    Fixed error

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from functools import partial
 from itertools import chain, filterfalse
 from re import match
 from inspect import isclass
-
+from lib3d_mec_ginac import *
 
 
 ######## Fixtures ########
@@ -62,3 +62,48 @@ def invalid_numeric_values():
     This fixture returns a list of objects which are not numeric values
     '''
     return 'foo', b'foo', (1, 2, 3), [1, 2, 3], {1, 2, 3}
+
+
+
+@pytest.fixture(scope='session')
+def system():
+    '''
+    This fixture creates a system with a few objects ( vectors, tensors, wrenches, ... )
+    for testing purposes ( it is used in the tests below )
+    The return system instance will have the next objects ( sorted by type ):
+    - symbols:
+        * parameters:      a, b
+        * inputs:          c
+        * joint_unknowns:  d
+        * coordinates:     (x, dx, ddx)
+        * aux_coordinates: (y, dy, ddy)
+
+    - bases:    bs
+    - vectors:  v
+    - points:   p
+    - tensors:  q
+    - solids:   s
+    - wrenches: w
+    - frames:   f
+    '''
+    sys = System()
+
+    # Fill system with symbols
+    sys.new_parameter('a')
+    sys.new_parameter('b')
+    sys.new_input('c')
+    sys.new_joint_unknown('d')
+    sys.new_coordinate('x')
+    sys.new_aux_coordinate('y')
+
+    # Fill system with geometric objects
+    sys.new_base('bs')
+    sys.new_vector('v', base='bs')
+    sys.new_vector('r')
+    sys.new_point('p', 'O', 'v')
+    sys.new_tensor('q', base='bs')
+    sys.new_solid('s', 'p', 'bs', 'a', 'v', 'q')
+    sys.new_frame('f', 'p', 'bs')
+    sys.new_wrench('w', 'v', 'r', 'p', 's', 'Constraint')
+
+    return sys
