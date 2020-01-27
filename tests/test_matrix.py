0325d734615a188b3cb9b27091c9f2c1fdb59f94
Vykstorm <victorruizgomezdev@gmail.com>
2020-01-27 16:18:26 +0100

    Added test to check the methods get & set in the class Matrix

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -6,6 +6,7 @@ Description: Unitary test for the class Matrix
 
 ######## Imports ########
 
+import pytest
 from lib3d_mec_ginac import *
 
 
@@ -28,18 +29,195 @@ def test_block():
     pass
 
 
-def test_getset_methods():
+def test_getset_methods(invalid_numeric_values):
     '''
     This is a test for the methods ``set`` & ``get`` in the class Matrix.
     '''
-    pass
+    m = Matrix([
+        [1, 2, 1],
+        [2, 4, 2],
+        [5, 5, 5]
+    ])
+    assert m.get(0, 0) == 1
+    assert m.get(0, 2) == 1
+    assert m.get(1, 1) == 4
+    assert m.get(2, 2) == 5
+    assert m.get(-1, 0) == 5
+    assert m.get(0, -2) ==  2
+    m.set(0, 0, 2)
+    m.set(0, 2, 3)
+    m.set(1, 1, 4)
+    m.set(2, 2, 5)
+    m.set(-1, 0, 6)
+    m.set(0, -2, 7)
+    assert m.get(0, 0) == 2
+    assert m.get(0, 2) == 3
+    assert m.get(1, 1) == 4
+    assert m.get(2, 2) == 5
+    assert m.get(-1, 0) == 6
+    assert m.get(0, -2) == 7
 
 
-def test_getset_operator():
+    # Same test but for a matrix with only one row
+    m = Matrix([ 1, 2, 3, 4 ])
+    assert m.get(0, 0) == 1
+    assert m.get(0, 1) == 2
+    assert m.get(0, 3) == 4
+    assert m.get(0, -2) == 3
+    assert m.get(0, -1) == 4
+    assert m.get(0) == 1
+    assert m.get(1) == 2
+    assert m.get(-1) == 4
+    m.set(0, 0, 5)
+    m.set(0, 1, 6)
+    m.set(2, 7)
+    m.set(-1, 8)
+    assert m.get(0, 0) == 5
+    assert m.get(0, 1) == 6
+    assert m.get(0, 3) == 8
+    assert m.get(0, -2) == 7
+    assert m.get(0, -1) == 8
+    assert m.get(0) == 5
+    assert m.get(1) == 6
+    assert m.get(-1) == 8
+
+    # Same as the above but for a matrix with only one column
+    m = Matrix([ 1, 2, 3, 4 ], shape=[4, 1])
+    assert m.get(0, 0) == 1
+    assert m.get(1, 0) == 2
+    assert m.get(3, 0) == 4
+    assert m.get(-2, 0) == 3
+    assert m.get(-1, 0) == 4
+    assert m.get(0) == 1
+    assert m.get(1) == 2
+    assert m.get(-1) == 4
+    m.set(0, 0, 5)
+    m.set(1, 0, 6)
+    m.set(2, 7)
+    m.set(-1, 8)
+    assert m.get(0, 0) == 5
+    assert m.get(1, 0) == 6
+    assert m.get(3, 0) == 8
+    assert m.get(-2, 0) == 7
+    assert m.get(-1, 0) == 8
+    assert m.get(0) == 5
+    assert m.get(1) == 6
+    assert m.get(-1) == 8
+
+    # If indices are wrongs, get & set raises IndexError
+    m = Matrix(shape=[3,3])
+    for i, j in [(3, 0), (0, 3), (3, 3), (-4, 0), (-4, 0)]:
+        with pytest.raises(IndexError):
+            m.get(i, j)
+
+        with pytest.raises(IndexError):
+            m.set(i, j, 0)
+
+    # If value is not numeric or something that can be converted to an expression,
+    # set raises ValueError
+    for x in invalid_numeric_values:
+        with pytest.raises(TypeError):
+            m.set(0, 0, x)
+
+
+
+
+def test_getset_operator(invalid_numeric_values):
     '''
     This is a test for the __getitem__ & __setitem__ metamethods in the class Matrix.
     '''
-    pass
+    m = Matrix([
+        [1, 2, 1],
+        [2, 4, 2],
+        [5, 5, 5]
+    ])
+    assert m[0, 0] == 1
+    assert m[0, 2] == 1
+    assert m[1, 1] == 4
+    assert m[2, 2] == 5
+    assert m[-1, 0] == 5
+    assert m[0, -2] ==  2
+    m[0, 0] = 2
+    m[0, 2] = 3
+    m[1, 1] = 4
+    m[2, 2] = 5
+    m[-1, 0] = 6
+    m[0, -2] = 7
+    assert m[0, 0] == 2
+    assert m[0, 2] == 3
+    assert m[1, 1] == 4
+    assert m[2, 2] == 5
+    assert m[-1, 0] == 6
+    assert m[0, -2] == 7
+
+
+    # Same test but for a matrix with only one row
+    m = Matrix([ 1, 2, 3, 4 ])
+    assert m[0, 0] == 1
+    assert m[0, 1] == 2
+    assert m[0, 3] == 4
+    assert m[0, -2] == 3
+    assert m[0, -1] == 4
+    assert m[0] == 1
+    assert m[1] == 2
+    assert m[-1] == 4
+    m[0, 0] = 5
+    m[0, 1] = 6
+    m[2] = 7
+    m[-1] = 8
+    assert m[0, 0] == 5
+    assert m[0, 1] == 6
+    assert m[0, 3] == 8
+    assert m[0, -2] == 7
+    assert m[0, -1] == 8
+    assert m[0] == 5
+    assert m[1] == 6
+    assert m[-1] == 8
+
+    # Same as the above but for a matrix with only one column
+    m = Matrix([ 1, 2, 3, 4 ], shape=[4, 1])
+    assert m[0, 0] == 1
+    assert m[1, 0] == 2
+    assert m[3, 0] == 4
+    assert m[-2, 0] == 3
+    assert m[-1, 0] == 4
+    assert m[0] == 1
+    assert m[1] == 2
+    assert m[-1] == 4
+    m[0, 0] = 5
+    m[1, 0] = 6
+    m[2] = 7
+    m[-1] = 8
+    assert m[0, 0] == 5
+    assert m[1, 0] == 6
+    assert m[3, 0] == 8
+    assert m[-2, 0] == 7
+    assert m[-1, 0] == 8
+    assert m[0] == 5
+    assert m[1] == 6
+    assert m[-1] == 8
+
+    # If indices are wrongs, __getitem__ & __setitem__ raises IndexError
+    m = Matrix(shape=[3,3])
+    for i, j in [(3, 0), (0, 3), (3, 3), (-4, 0), (-4, 0)]:
+        with pytest.raises(IndexError):
+            m[i, j]
+
+        with pytest.raises(IndexError):
+            m[i, j] = 0
+
+    # If value is not numeric or something that can be converted to an expression,
+    # __setitem__ raises ValueError
+    for x in invalid_numeric_values:
+        with pytest.raises(TypeError):
+            m[0, 0] = x
+
+
+
+
+
+
+
 
 
 def test_iterator():
