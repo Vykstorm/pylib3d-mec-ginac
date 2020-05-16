7aec519d6f616c2c883e90a359d6cc70f44e194d
Vykstorm <victorruizgomezdev@gmail.com>
2020-05-16 13:27:18 +0200

    Added instructions to check if python & pip are installed

--- install.sh
+++ install.sh
@@ -1,4 +1,44 @@
 
+# Verify python & pip are installed
+if [ -z $(which python) ]
+then
+    echo "Python is not installed yet!"
+    exit 1
+fi
+
+if [ -z $(which pip) ]
+then
+    echo "Pip is not installed yet!"
+    exit 1
+fi
+
+
+# Verify python version
+PYTHON_VERSION=$(python -c "import sys; print(str(sys.version_info.major) + '.' + str(sys.version_info.minor))")
+
+if [ "$PYTHON_VERSION" != "3.7" ]
+then
+    echo "Python version must be 3.7.*"
+    exit 1
+fi
+
+# Verify pip version
+if [ -z "$(pip show pip | grep Location | grep 3.7)" ]
+then
+    echo "Invalid pip!"
+    exit 1
+fi
+
+# Is lib3d-mec-ginac already installed ?
+
+if [ "$(pip freeze | grep lib3d-mec-ginac)" ]
+then
+    echo "pylib3d-mec-ginac already installed!"
+    exit 1
+fi
+
+
+
 # Install dependencies
 sudo apt update && \
 sudo apt install -y \
