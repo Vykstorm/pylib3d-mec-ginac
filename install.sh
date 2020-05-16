f260716c1686a80ce1764a710dcdda3b8911dc47
Vykstorm <victorruizgomezdev@gmail.com>
2020-05-16 12:38:29 +0200

    Created bash script to install this library

---
+++ install.sh
@@ -0,0 +1,25 @@
+
+# Install dependencies
+apt update && \
+apt install -y \
+    g++ \
+    mesa-utils \
+    libsm6 \
+    libxrender1 \
+    libfontconfig \
+    git \
+    curl \
+    libgl-dev \
+    python3-tk \
+&& apt clean
+
+# Clone repository and install library
+rm -rf pylib3d-mec-ginac \
+&& git clone https://github.com/Vykstorm/pylib3d-mec-ginac.git --branch stable \
+&& cd pylib3d-mec-ginac \
+&& pip install -r requirements.txt \
+&& python setup.py install \
+&& cd ..
+
+# Verify the installation
+python -c "import lib3d_mec_ginac"
